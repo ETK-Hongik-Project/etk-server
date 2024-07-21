35bbe7658c4675e8838605a22e1acd27cb9debd6
Min Jeon <mulsoap0504@gmail.com>
2024-07-21 22:34:30 +0900

    Refactor: dto NotBlank 적용

--- src/main/java/Hongik/EyeTracking/user/dto/request/CreateUserRequestDto.java
+++ src/main/java/Hongik/EyeTracking/user/dto/request/CreateUserRequestDto.java
@@ -1,15 +1,20 @@
 package Hongik.EyeTracking.user.dto.request;
 
 import Hongik.EyeTracking.user.domain.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateUserRequestDto {
+    @NotBlank(message = "이름이 입력되지 않았습니다")
     private String name;
+    @NotBlank(message = "아이디가 입력되지 않았습니다")
     private String username;
+    @NotBlank(message = "비밀번호가 입력되지 않았습니다")
     private String password;
+    @NotBlank(message = "이메일이 입력되지 않았습니다")
     private String email;
 
     @Builder
