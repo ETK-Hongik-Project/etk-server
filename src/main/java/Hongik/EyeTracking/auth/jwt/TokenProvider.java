5765bc2a89437f64681d152bb5ad62b127e79b81
Min Jeon <mulsoap0504@gmail.com>
2024-09-09 14:58:38 +0900

    Update TokenProvider.java

--- src/main/java/Hongik/EyeTracking/auth/jwt/TokenProvider.java
+++ src/main/java/Hongik/EyeTracking/auth/jwt/TokenProvider.java
@@ -38,7 +38,7 @@ public class TokenProvider implements InitializingBean {
                          CustomUserDetailsService customUserDetailsService) {
         this.secret = secret;
         this.validationTime = validationTime * 60000 * 60;  // accessToken 만료: validationTime 시간
-        this.refreshTokenValidationTime = validationTime * 24 * 7 * 60000;
+        this.refreshTokenValidationTime = validationTime * 60000 * 60 * 7;
         this.customUserDetailsService = customUserDetailsService;
     }
 
